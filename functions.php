566b1ea452ec2f2400f6a2be76d98bb2428984cd
Евгения <eremeevazhenya@gmail.com>
2015-10-06 22:20:46 +0300

    Функции для регистрации

---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+require_once ('connect.php');
+require_once ('user_functions.php');
+
+
+/**
+ * Возвращает список статусов
+ * @return array
+ */
+function getStatuses()
+{
+    global $handle;
+    $query = "SELECT name_status,id_status
+	       FROM statuses
+	       ORDER BY name_status ASC";
+    $result = $handle->query($query);
+    $res = $result->fetch_all(MYSQLI_ASSOC);
+    return $res;
+}
+
+function getCities()
+{
+    global $handle;
+    $query = "SELECT name_city,id_city
+	       FROM cities
+	       ORDER BY name_city ASC";
+    $result = $handle->query($query);
+    $res = $result->fetch_all(MYSQLI_ASSOC);
+    return $res;
+}
+
+function getStreets()
+{
+    global $handle;
+    $query = "SELECT name_street,id_street
+	       FROM streets
+	       ORDER BY name_street ASC";
+    $result = $handle->query($query);
+    $res = $result->fetch_all(MYSQLI_ASSOC);
+    return $res;
+}
+
